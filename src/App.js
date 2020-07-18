83ccfb6fa1945ecfc4aff732e244828c7c527812
hafiz salman <hmsalmans1@gmail.com>
2020-07-18 10:43:16 +0500

    navbar ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Aboutme from "./components/Aboutme";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <h1 className="navbar-brand disabled" to="#">
+            Salman
+          </h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link " to="/Aboutme">
+                AboutMe 
+              </Link>
+              <Link className="nav-item nav-link" to="/Projects">
+                Projects
+              </Link>
+              <Link className="nav-item nav-link" to="/Contact">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </nav>
+
+        <div>
+          <Switch>
+            <Route exact path="/Aboutme" component={Aboutme} />
+            <Route exact path="/Contact" component={Contact} />
+
+            <Route exact path="/Projects" component={Projects} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
