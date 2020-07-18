2cc726412617096560fa7dcf6637da592df9c510
hafiz salman <hmsalmans1@gmail.com>
2020-07-18 18:26:21 +0500

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Aboutme from "./components/Aboutme";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-
+import Navbar from "./components/Navbar";
 class App extends React.Component {
   render() {
     return (
       <Router>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <h1 className="navbar-brand disabled" to="#">
-            Salman
-          </h1>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <Link className="nav-item nav-link " to="/Aboutme">
-                AboutMe 
-              </Link>
-              <Link className="nav-item nav-link" to="/Projects">
-                Projects
-              </Link>
-              <Link className="nav-item nav-link" to="/Contact">
-                Contact
-              </Link>
-            </div>
-          </div>
-        </nav>
-
+        <Navbar />
         <div>
           <Switch>
             <Route exact path="/Aboutme" component={Aboutme} />
             <Route exact path="/Contact" component={Contact} />
-
             <Route exact path="/Projects" component={Projects} />
           </Switch>
         </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Navbar extends Component {
+    render () {
+        return(
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+          <h1 className="navbar-brand disabled" to="#">
+           <span style={{color: "lime"}}>Salman's Porfolio</span>
+          </h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-item nav-link" to="/Aboutme">
+                AboutMe 
+              </Link>
+              <Link className="nav-item nav-link" to="/Projects">
+                Projects
+              </Link>
+              <Link className="nav-item nav-link" to="/Contact">
+                Contact
+              </Link>
+            </div>
+          </div>
+        </nav>
+        </div>
+        )
+    }
+}
+
+export default Navbar;
